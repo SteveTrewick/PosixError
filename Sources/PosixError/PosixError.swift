5a6f3be53ed16be0193723d37232cc44becfc4ed
Steve Trewick <steve.trewick@icloud.com>
2024-01-19 13:51:20 +0000

    extend

--- Sources/PosixError/PosixError.swift
+++ Sources/PosixError/PosixError.swift
@@ -1,6 +1,18 @@
-public struct PosixError {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import Trace
 
-    public init() {
-    }
+public struct PosixError {
+  public let errno    : Int32
+  public let strerror : String
+  
+  public init() {
+    self.errno    = Darwin.errno
+    self.strerror = String( cString: Darwin.strerror(errno))
+  }
+}
+
+extension Trace {
+  public static func posix(file: String = #file, line: Int = #line, function: String = #function, _ source: Any, tag: String = "") -> Trace {
+      Trace(file: file, line: line, function: function, source, tag: tag, context: PosixError())
+  }
 }
