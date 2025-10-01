4af77b2754b5f85c7edcea2faf283828f7a1da31
Steve Trewick <steve.trewick@icloud.com>
2025-10-01 21:29:15 +0100

    hopefully add linux compat so my shit will compile on codex cloud

--- Sources/PosixError/PosixError.swift
+++ Sources/PosixError/PosixError.swift
@@ -1,13 +1,26 @@
 import Foundation
 @_exported import Trace
 
+#if canImport(Glibc)
+import Glibc
+#endif
+
+
 public struct PosixError {
   public let errno    : Int32
   public let strerror : String
   
   public init() {
-    self.errno    = Darwin.errno
+    self.errno    = PosixError.posixerrno()
     self.strerror = String( cString: Darwin.strerror(errno))
+  }
+  
+  static func posixerrno() -> Int32 {
+    #if canImport(Glibc)
+    return Glibc.errno
+    #else
+    return Darwin.errno
+    #endif
   }
 }
 
@@ -15,4 +28,7 @@ extension Trace {
   public static func posix(file: String = #file, line: Int = #line, function: String = #function, _ source: Any, tag: String = "") -> Trace {
       Trace(file: file, line: line, function: function, source, tag: tag, context: PosixError())
   }
+  
+  
+  
 }
